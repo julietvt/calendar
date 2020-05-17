054f485b7ba0c1fe5e3a110e8df40c1e3cc01d5c
julietvt <freshitpt@gmail.com>
2020-05-17 12:53:29 +0300

    create model calendar

---
+++ assets/model/Day.js
@@ -0,0 +1,21 @@
+'use strict';
+class Day{
+    constructor(date) {
+        this._date = date;
+        this._isCurrent = Day.isCurrent;
+    }
+    get date(){
+        return this._date;
+    }
+    set date(value){
+        this._date = value;
+        this._isCurrent = Day.isCurrent(value);
+    }
+    get isCurrent(){
+        return this._isCurrent;
+    }
+    static isCurrent(date){
+        return date.toDateString() === Day.currentDate.toDateString();
+    }
+    static currentDate = new Date();
+}
---
+++ assets/model/index.js
@@ -0,0 +1,9 @@
+'use strict';
+import Day from "./Day"
+import Week from "./Week"
+import Month from "./Month"
+export default {
+    Day,
+    Week,
+    Month,
+}
